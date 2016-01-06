baab8cd47b7b322900a3f675ac7192b8353065eb
Sydothekido11 <snem8901@740s-pl.local>
2016-01-06 12:15:54 -0700

    Added new files and the structure for them.

---
+++ AboutMe/JobViewController.swift
@@ -0,0 +1,16 @@
+//
+//  JobViewController.swift
+//  AboutMe
+//
+//  Created by Nemelka, Sydney on 1/6/16.
+//  Copyright © 2016 Nemelka, Sydney. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class JobViewContoller :UIViewController
+{
+    
+    
+}
---
+++ AboutMe/KpopViewController.swift
@@ -0,0 +1,15 @@
+//
+//  KpopViewController.swift
+//  AboutMe
+//
+//  Created by Nemelka, Sydney on 1/6/16.
+//  Copyright © 2016 Nemelka, Sydney. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class KpopViewController : UIViewController
+{
+    
+}
